bbecf078bbaac9cf5199485da5c942d75a48f310
Pat McCarthy <pat@dwellable.com>
2020-08-28 15:30:00 -0500

    adding some book content and ignore files

---
+++ src/content/book-content.js
@@ -0,0 +1,103 @@
+module.exports = {
+    title: `If You Give a Dev a Metric`,
+    subTitle: `A story about where application obervability can take you`,
+    author: `@patdmc`,
+    pages: [
+        {
+            line: 'If you give a dev a metric...',
+            page: 1,
+            note: 'just show json'
+        },
+        {
+            line: `They're going to want to graph it.`,
+            page: 2,
+            note: 'then show a simple graph'
+        },
+        {
+            line: `And then, once they graph it, they're going to want to decorate the graphq so they can easily see if everything is OK.`,
+            page: 3,
+            note: 'now show a graph with yellow and red areas'
+        },
+        {
+            line: `The dev will love their graph, and look at it every day. And when they release code they check the graph to make sure the app is working as expected.`,
+            page: 4,
+            note: 'show a happy dev shipping code by pressing a button while looking at the graph'
+        },
+        {
+            line: `But after doing this for a while, the dev is going to want to simplify...`,
+            page: 5,
+            note: 'replace the graph with a stoplight'
+        },
+        {
+            line: `And once it's that simple, the dev will want to set up an alert, so they don't always have to check the graph to know the app is OK`,
+            page: 7,
+            note: 'red light, dev getting paged on phone'
+        },
+        {
+            line: `But with only one alert, sometimes bad stuff happens without the dev knowing...`,
+            page: 8,
+            note: 'dev with phone showing nothing, light greem, PC Load Letter for the app'
+        },
+        {
+            line: `And the dev is going to want more metrics.`,
+            page: 9,
+            note: 'dev with dashboard with lots of metrics'
+        },
+        {
+            line: `The dev is happy, but sometimes when things go bad on their app, its another team's app that caused it`,
+            page: 10,
+            note: 'dev with dashboard showing other app failing'
+        },
+        {
+            line: `And the dev is going to show that team their dashboard and monitors, and get them to set up some too`,
+            page: 11,
+            note: 'dev showing off their dashboard'
+        },
+        {
+            line: `And the other devs love it, and start getting creative making their own`,
+            page: 12,
+            note: 'more devs, with custom crazy dashboards shaped like rocketships and unicorns'
+        },
+        {
+            line: `And everyone is happy, until something systemic goes wrong and everyone struggles to see the root cause`,
+            page: 13,
+            note: 'devs pointing fingers'
+        },
+        {
+            line: `This happens a few times and the devs realize, it's taking too long to figure out the root cause, especially if one team or another doesn't respond`,
+            page: 14,
+            note: 'devs frantic while another dev sleeps'
+        },
+        {
+            line: `But the devs dont all want to wake up and scramble every time something goes wrong, so they decide to make a single dashboard they all share`,
+            page: 15,
+            note: 'devs unify their dashboard'
+        },
+        {
+            line: `Which is great unti they realize when something goes wrong, they still have to page a specific team`,
+            page: 16,
+            note: 'devs still frantic while another dev sleeps'
+        },
+        {
+            line: `So then they write production runbooks, so they can cover more issues with a single support rotation and they are on call less`,
+            page: 17,
+            note: 'one dev handling an incident while other devs play'
+        },        
+        {
+            line: `And this works reduces production overhead, freeing up the devs to build more new features`,
+            page: 18,
+            note: 'Devs building unicorns'
+        },
+        {
+            line: `And once they've shipped all these new features`,
+            page: 19,
+            note: 'unicorns shipping'
+        },
+        {
+            line: `The devs are going to want more metrics (but this time with graphs and production runbooks)`,
+            page: 20,
+            note: 'Back to the first page'
+        },
+
+    ]
+};
